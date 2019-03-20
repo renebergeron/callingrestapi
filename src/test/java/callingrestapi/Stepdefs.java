6d5fc093d6565085e4de07cdc5bc99be7b35bdbf
rbergeron <rbergeron@solocal.com>
2019-03-20 15:32:03 +0100

    Refactoring

--- src/test/java/callingrestapi/Stepdefs.java
+++ src/test/java/callingrestapi/Stepdefs.java
@@ -13,44 +13,39 @@ import java.net.URL;
 
 public class Stepdefs {
 
+	HttpURLConnection con;
+    StringBuffer content = new StringBuffer();
+	
     @Given("^restapiurl is \"([^\"]*)\"$")
     public void restapiurl_is(String restapiurl) throws Exception {
         // Write code here that turns the phrase above into concrete actions
         System.out.println("restapiurl: " + restapiurl);
         URL url = new URL(restapiurl);
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         
         int status = con.getResponseCode();
         System.out.println("Status = " + status);
-        
+        assertEquals(200, status);
+    }
+
+    @When("^calling rest api$")
+    public void calling_rest_api() throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
         String inputLine;
-        StringBuffer content = new StringBuffer();
+
         while((inputLine = in.readLine()) != null) {
         	content.append(inputLine);
         }
         in.close();
         con.disconnect();
         
-        System.out.println("result is => " + content);
-//        throw new PendingException();
-    }
-
-    @When("^calling rest api$")
-    public void calling_rest_api() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("On devra caller l'url");
-
-//        throw new PendingException();
+        assertNotNull(content);
     }
 
     @Then("^server should handle it an return success status$")
     public void server_should_handle_it_an_return_success_status() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("Faudra voir quoi faire ici");
-
-//        throw new PendingException();
+    	System.out.println("The content of the API is => " + content);
     }
 
 }
