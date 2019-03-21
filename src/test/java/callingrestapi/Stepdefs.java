38ebe28f709f7b8cfadf5ae5ff68cf8b3f7add0f
rbergeron <rbergeron@solocal.com>
2019-03-21 18:22:27 +0100

    Petits avances

--- src/test/java/callingrestapi/Stepdefs.java
+++ src/test/java/callingrestapi/Stepdefs.java
@@ -1,6 +1,5 @@
 package callingrestapi;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
@@ -10,6 +9,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Stepdefs {
 
@@ -43,9 +46,37 @@ public class Stepdefs {
         assertNotNull(content);
     }
 
-    @Then("^server should handle it an return success status$")
-    public void server_should_handle_it_an_return_success_status() throws Exception {
+    @Then("^valid the json formant$")
+    public void valid_the_json_formant() throws Exception {
+    	JsonArray arr = new JsonParser().parse(content.toString()).getAsJsonArray();
+
+    	assertNotNull("The fucking object shouldn't be null", arr);
+        for (int i = 0; i < arr.size(); i++) {
+          JsonObject obj = arr.get(i).getAsJsonObject();
+          String productId = obj.get("idProduit").getAsString();
+          String productCode = obj.get("codeProduit").getAsString();
+          String codePack = obj.get("codePack").getAsString();
+//          String libelleProduit = obj.get("libelleProduit").getAsString();
+//          String epjAnnonceur = obj.get("epjAnnonceur").getAsString();
+//          String epjNegociateur = obj.get("epjNegociateur").getAsString();
+//          String epjBeneficiaire = obj.get("epjBeneficiaire").getAsString();
+//          String etatProduit = obj.get("etatProduit").getAsString();
+          System.out.println("productId : " + productId);
+          System.out.println("codeProduit : " + productCode);
+//          System.out.println("codePack : " + codePack);
+//          System.out.println("epjAnnonceur : " + epjAnnonceur);
+//          System.out.println("epjNegociateur : " + epjNegociateur);
+//          System.out.println("epjBeneficiaire : " + epjBeneficiaire);
+//          System.out.println("etatProduit : " + etatProduit);
+
+        }
     	System.out.println("The content of the API is => " + content);
+    	// Do somme validation on the json format if possible
     }
 
+    @Then("^tester les donnees$")
+    public void tester_les_donnees() throws Exception {
+    	System.out.println("Voir comment valide les datas si il y a lieu!");
+    	// Is it necessairy to tests the values since the data is not fix
+    }
 }
--- src/test/resources/callingrestapi/callingrestapi.feature
+++ src/test/resources/callingrestapi/callingrestapi.feature
@@ -5,4 +5,5 @@ Feature: Call a REST API
     Scenario: We first try to get a result calling rest api
     Given restapiurl is "http://localhost:8080/cxf/rest/product/offer/GetSubscribedOffers/00000006"
     When calling rest api
-    Then server should handle it an return success status
+    Then valid the json formant
+    And tester les donnees
